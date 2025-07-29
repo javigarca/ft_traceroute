9956e810de2684629f9bf94f201ba142c12533b3
Javi <javigarc@student.42.urduliz.com>
2025-07-29 21:52:23 +0200

    listo mandatory

--- sources/ft_traceroute.c
+++ sources/ft_traceroute.c
@@ -67,13 +67,15 @@ int main (int argc, char **argv)
     print_infofn(1, stdout, "ft_traceroute to %s (%s) %d hops max %d bytes packets", opts.target.hostname, opts.target.ip_str, PAYLOAD_SIZE, WIRE_BYTES);
 
     for (size_t ttl = 1; ttl <= NUM_TTL && g_interrupted == 0; ttl++){
+        char    last_ip[INET_ADDRSTRLEN] = "";
+
          if (setsockopt(socket_send, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) < 0)
             error_exit(EXIT_FAILURE, 0, "Error setting TTL: %u", ttl);
         print_infof(1, stdout, "%u ", ttl);
     
         for (size_t probe = 0; probe < NUM_PROBES; probe++) {
             struct  timeval t_send, t_recv, time_out;
-            double    t_rtt_ms;
+            double  t_rtt_ms;
             int     response = 0;
             fd_set  rfds;
 
@@ -85,15 +87,16 @@ int main (int argc, char **argv)
                 time_out.tv_sec  = DEF_TIME_OUT_S;
                 time_out.tv_usec = DEF_TIME_OUT_U;
                 
-                // imprimir el nombre de 1  _gateway (192.168.1.1)  1.649 ms  4.064 ms  4.113 ms
-                // 2  10.0.29.139 (10.0.29.139)  4.735 ms  4.647 ms  4.869 ms
-                if (probe == 0){
-                    print_infof(1, stdout," %s (%s) ", opts.hop.ip_str, opts.hop.ip_str);
-                }
                 int reply = select(socket_recv + 1, &rfds, NULL, NULL, &time_out);
      
-                if (reply < 0)
-                    error_exit(EXIT_FAILURE, errno, "select");
+                if (reply < 0){
+                    if (errno == EINTR) {
+                        g_interrupted = 1;
+                        break;
+                    } else {
+                        error_exit(EXIT_FAILURE, errno, "select");
+                    }
+                }
                 if (reply == 0) {
                     print_infof(1, stdout," * ");    
                     break;
@@ -101,13 +104,17 @@ int main (int argc, char **argv)
                 //analisis de paquete
                 response = receive_packet(socket_recv, seq, &opts); 
                 print_infofn(opts.debug, stderr, "RESPONSE: %d", response);
-                if (response > 0) {
-                    gettimeofday(&t_recv, NULL);
-                    t_rtt_ms = (t_recv.tv_sec  - t_send.tv_sec) * 1000.0 + (t_recv.tv_usec - t_send.tv_usec) / 1000.0;
-                    print_infof(1, stdout,"%.3f ms ", t_rtt_ms);
-                    if (response == 2 ){
-                        g_interrupted = 1;
-                    }
+                    if (response > 0) {
+                        if (strcmp(opts.hop.ip_str,last_ip) != 0){
+                            strcpy(last_ip, opts.hop.ip_str);
+                            print_infof(1, stdout," %s (%s) ", last_ip, last_ip);
+                        }
+                        gettimeofday(&t_recv, NULL);
+                        t_rtt_ms = (t_recv.tv_sec  - t_send.tv_sec) * 1000.0 + (t_recv.tv_usec - t_send.tv_usec) / 1000.0;
+                        print_infof(1, stdout,"%.3f ms ", t_rtt_ms);
+                        if (response == 2 ){
+                            g_interrupted = 1;
+                        }
                 }
             }
             seq++;
